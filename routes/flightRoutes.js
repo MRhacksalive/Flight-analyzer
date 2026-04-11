8a57ce8a3108ee3be1459681269cc4d999efb557
MRhacksalive <adityakroy123@gmail.com>
2026-04-11 05:55:32 +0530

    initial

--- routes/flightRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getFlightData } = require('../controllers/flightController');
-
-router.get('/:flightNumber', getFlightData);
-
-module.exports = router;
